76d0d448df6a32ea873f569fded10ac93f33db93
Andy Wickert <andy@northernwidget.com>
2014-05-13 14:48:33 -0600

    Live version from Andy's arduino-1.0.5 libraries folder to github

--- examples/alog_no_sensors/alog_no_sensors.ino
+++ examples/alog_no_sensors/alog_no_sensors.ino
@@ -1,29 +1,3 @@
-/*
-alog_no_sensors
-This is a framework code for using the Northern Widget "Logger" library,
-with appropriate places to add in analog and digital sensors to read.
-
-# LICENSE: GNU GPL v3
-
-alog_no_sensors.ino is part of Logger, an Arduino library written by Andrew D. 
-Wickert.
-Copyright (C) 2011-2013, Andrew D. Wickert and Northern Widget LLC
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
 #include "Logger.h"
 
 // Libraries don't seem to automatically come along
