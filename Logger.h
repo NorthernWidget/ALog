c233b019be39f77e145229a2bfdb3ec2a1e4e5fe
Andy Wickert <andy@northernwidget.com>
2016-06-15 18:09:46 -0500

    Remove ATMega644P/1284P pinout

--- Logger.h
+++ Logger.h
@@ -158,34 +158,4 @@ class Logger {
 };
 
 #endif  
-    
-///////////////
-// REFERENCE //
-///////////////
-
-// Reference pinout for ATmega644/1284
-// INT2 not currently working
-//
-//                      +---\/---+
-//          (D 0) PB0  1|        |40  PA0 (AI 0 / D24)
-//          (D 1) PB1  2|        |39  PA1 (AI 1 / D25)
-//INT2-BAD  (D 2) PB2  3|        |38  PA2 (AI 2 / D26)
-//      PWM (D 3) PB3  4|        |37  PA3 (AI 3 / D27)
-//      PWM (D 4) PB4  5|        |36  PA4 (AI 4 / D28)
-//     MOSI (D 5) PB5  6|        |35  PA5 (AI 5 / D29)
-//PWM  MISO (D 6) PB6  7|        |34  PA6 (AI 6 / D30)
-//PWM   SCK (D 7) PB7  8|        |33  PA7 (AI 7 / D31)
-//                RST  9|        |32  AREF
-//                VCC 10|        |31  GND 
-//                GND 11|        |30  AVCC
-//              XTAL2 12|        |29  PC7 (D 23)
-//              XTAL1 13|        |28  PC6 (D 22)
-//     RX0 (D 8)  PD0 14|        |27  PC5 (D 21) TDI
-//     TX0 (D 9)  PD1 15|        |26  PC4 (D 20) TDO
-//INT0 RX1 (D 10) PD2 16|        |25  PC3 (D 19) TMS
-//INT1 TX1 (D 11) PD3 17|        |24  PC2 (D 18) TCK
-//     PWM (D 12) PD4 18|        |23  PC1 (D 17) SDA
-//     PWM (D 13) PD5 19|        |22  PC0 (D 16) SCL
-//     PWM (D 14) PD6 20|        |21  PD7 (D 15) PWM
-//                      +--------+
 
