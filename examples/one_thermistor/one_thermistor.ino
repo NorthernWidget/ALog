8faaf8e8cf8a22a9c981320e5425686f8a9928b0
Andy Wickert <andy@northernwidget.com>
2017-02-24 09:12:11 -0600

    No days, thermistor update

--- examples/one_thermistor/one_thermistor.ino
+++ examples/one_thermistor/one_thermistor.ino
@@ -10,14 +10,17 @@ char* fileName = "SC01.txt"; // Name of file for logged data: 8.3 format (e.g,
                              // ABCDEFGH.TXT); <= 8 chars before ".txt" is OK
 
 //Setup logging interval here, may setup more than one variable. 
-//Minimum interval = 1 sec, maximum interval is 1 second short of 7 days.  
+//Minimum interval = 1 sec, maximum interval is 23 hours, 59 minutes, 59 seconds
 int Log_Interval_Seconds = 30; //Valid range is 0-59 seconds
 int Log_Interval_Minutes = 0; //Valid range is 0-59 minutes
 int Log_Interval_Hours = 0; //Valid range is 0-23 hours
-int Log_Interval_Days = 0; //Valid range is 0-6 days
 bool external_interrupt = false; // e.g., rain gage
 
-void setup(){logger.initialize(dataLoggerName, fileName, Log_Interval_Days, Log_Interval_Hours, Log_Interval_Minutes, Log_Interval_Seconds, external_interrupt);logger.setupLogger();}
+void setup(){
+  logger.initialize(dataLoggerName, fileName,
+    Log_Interval_Hours, Log_Interval_Minutes, Log_Interval_Seconds, 
+    external_interrupt);logger.setupLogger();
+}
 
 void loop(){
 // ***************************************** 
@@ -36,7 +39,10 @@ logger.startLogging();  // Wake up and initialize
 
 logger.startAnalog();
 
-logger.thermistorB(10000, 3950, 10000, 25, 0);
+// CanTherm small bead
+//logger.thermistorB(10000, 3950, 10000, 25, 0);
+// CanTherm expoxy bead: CWF1B103F3380
+logger.thermistorB(10000, 3380, 10000, 25, 0);
 
 logger.endAnalog();
 
@@ -44,10 +50,9 @@ logger.endAnalog();
 // INSERT DIGITAL SENSOR READING COMMANDS HERE!
 //
 
-// NOTE: THE BUFFER SIZE IS (CHECK ON THIS!!!!!!!!!!!!!!) 256 BYTES;
+// NOTE: THE BUFFER SIZE IS 512 BYTES;
 // run "logger.bufferWrite" if you think you are approaching this limit.
 // Otherwise, the buffer will overflow and I'm not sure what will happen.
-
 
 // ****** DO NOT EDIT BELOW THIS LINE ****** 
 
