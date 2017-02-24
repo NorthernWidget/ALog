62828be792d107e01106fbbd5320a9d3f2df2e5e
Andy Wickert <andy@northernwidget.com>
2017-02-24 09:04:23 -0600

    No days

--- examples/ALog_no_sensors/ALog_no_sensors.ino
+++ examples/ALog_no_sensors/ALog_no_sensors.ino
@@ -10,16 +10,15 @@ char* fileName = "T01.txt"; // Name of file for logged data: 8.3 format (e.g,
                             // ABCDEFGH.TXT); <= 8 chars before ".txt" is OK
 
 //Setup logging interval here, may setup more than one variable. 
-//Minimum interval = 1 sec, maximum interval is 1 second short of 7 days.  
-int Log_Interval_Seconds = 0; //Valid range is 0-59 seconds
+//Minimum interval = 1 sec, maximum interval is 23 hours, 59 minutes, 59 seconds
+int Log_Interval_Seconds = 10; //Valid range is 0-59 seconds
 int Log_Interval_Minutes = 0; //Valid range is 0-59 minutes
 int Log_Interval_Hours = 0; //Valid range is 0-23 hours
-int Log_Interval_Days = 0; //Valid range is 0-6 days
 // external_interrupt is true for a tipping bucket rain gauge
 bool external_interrupt = false;
 
 void setup(){
-  logger.initialize(dataLoggerName, fileName, Log_Interval_Days, \
+  logger.initialize(dataLoggerName, fileName,
       Log_Interval_Hours, Log_Interval_Minutes, Log_Interval_Seconds,
       external_interrupt);
   logger.setupLogger();
