1541429b6be99660c8b1132c8b3ac621bfafdcb4
Andy Wickert <andy@northernwidget.com>
2017-06-01 21:46:45 +0100

    branch to gh-pages without deleting branch

--- branchDoc.sh
+++ branchDoc.sh
@@ -1,5 +1,13 @@
-# Run once -- unless we want to do delete and remake the branch each time
-#git checkout --orphan gh-pages
-#git add doc/html && git commit -m "Initial doc subtree commit -- gh-pages"
+#! /bin/sh
+
+git checkout master
+doxygen doxygen_Logger.cfg
+
+git checkout --orphan gh-pages # first time only
+git checkout gh-pages doc/html # all other times
+
+git add doc/html # even needed at this point?
+git commit -m "Documentation update (doxygen): gh-pages"
 git subtree push --prefix doc/html origin gh-pages
-git checkout master -f
+
+git checkout master
