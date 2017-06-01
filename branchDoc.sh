39d262765ff5ec1bd0d9ffd9402327ce9eaa07aa
Andy Wickert <andy@northernwidget.com>
2017-06-01 22:17:15 +0100

    Keep original figures as a subfolder

--- branchDoc.sh
+++ branchDoc.sh
@@ -3,19 +3,20 @@
 git checkout master
 doxygen doxygen_Logger.cfg
 
+cp -r doc/figures doc/html/
+
 git checkout --orphan gh-pages # first time only
 git checkout gh-pages doc/html # all other times
-git checkout gh-pages doc/figures # all other times
-
+#git checkout gh-pages doc/figures # all other times
 
 git checkout gh-pages
 
-git add -f doc/html # even needed at this point?
+git add doc/html # even needed at this point?
 git commit -m "Documentation update (doxygen): gh-pages"
 git subtree push --prefix doc/html origin gh-pages
 
-git add -f doc/figures
-git commit -m "source figures"
-git push origin master
+#git add -f doc/figures
+#git commit -m "source figures"
+#git push origin master
 
 git checkout master
