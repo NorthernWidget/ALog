ad2d03710f74a1f422c18a57bf29907ffca84554
Andy Wickert <andy@northernwidget.com>
2017-06-02 20:07:46 +0100

    Unnecessary but good to have a log

--- buildDoxygen.sh
+++ buildDoxygen.sh
@@ -4,4 +4,16 @@ pandoc -f markdown_github -t markdown README.md > README_doxygen.md
 perl -CSDA -plE 's/\s/ /g' README_doxygen.md # replace Unicode whitespace with ASCII
 sed -i -e 's/\ {.c/{.c/g' README_doxygen.md # rm space before {.cpp} for syntax highlighting
 doxygen doxygen_Logger.cfg
-sh branchDoc.sh
+#sh branchDoc.sh
+
+
+
+# Testing HTML --> TeX
+
+echo "/*! \mainpage ALog Guide" > intro.dox
+echo "" >> intro.dox
+pandoc -f markdown_github -t html README.md >> intro.dox
+echo "" >> intro.dox
+echo " */" >> intro.dox
+doxygen doxygen_Logger_TeX.cfg
+
