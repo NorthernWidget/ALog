8e58c72b715c730f4e400a40b1f808b0789e416a
Andy Wickert <andy@northernwidget.com>
2017-06-02 20:13:38 +0100

    Build full help

--- buildDoxygen.sh
+++ buildDoxygen.sh
@@ -1,19 +1,20 @@
 #! /bin/sh
 
+# Set up the Markdown
 pandoc -f markdown_github -t markdown README.md > README_doxygen.md
 perl -CSDA -plE 's/\s/ /g' README_doxygen.md # replace Unicode whitespace with ASCII
 sed -i -e 's/\ {.c/{.c/g' README_doxygen.md # rm space before {.cpp} for syntax highlighting
+
+# Create doxygen files: HTML and PDF
 doxygen doxygen_Logger.cfg
-#sh branchDoc.sh
+cd doc/latex
+./Make.sh
 
+# Move PDF into HTML folder so it appears in branch but doesn't have
+# tracked history (would be too much overhead)
+cp refman.pdf ../html/refman.pdf
 
-
-# Testing HTML --> TeX
-
-echo "/*! \mainpage ALog Guide" > intro.dox
-echo "" >> intro.dox
-pandoc -f markdown_github -t html README.md >> intro.dox
-echo "" >> intro.dox
-echo " */" >> intro.dox
-doxygen doxygen_Logger_TeX.cfg
+# Upload help files to branch
+cd ../..
+./branchDoc.sh
 
