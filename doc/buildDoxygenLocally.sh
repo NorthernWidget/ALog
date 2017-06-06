adcd78bfd93efa997bc4e9a24e2fcb48ab93d69c
Andy Wickert <andy@northernwidget.com>
2017-06-06 18:51:02 +0100

    Scripts to build documentation in documentation root folder

--- doc/buildDoxygenLocally.sh
+++ doc/buildDoxygenLocally.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 # Set up the Markdown
-pandoc -f markdown_github -t markdown README.md > README_doxygen.md
+pandoc -f markdown_github -t markdown ../README.md > README_doxygen.md
 perl -CSDA -plE 's/\s/ /g' README_doxygen.md # replace Unicode whitespace with ASCII
 sed -i -e 's/\ {.c/{.c/g' README_doxygen.md # rm space before {.cpp} for syntax highlighting
 
 # Create doxygen files: HTML and PDF
 doxygen doxygen_Logger.cfg
-cd doc/latex
+cd latex
 ./Make.sh
 
 # Move PDF into HTML folder so it appears in branch but doesn't have
